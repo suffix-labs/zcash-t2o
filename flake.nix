4516baa76887550f8d6398878d550e72f39dc6aa
Thomas Butler <trbiv@proton.me>
2025-11-23 18:02:50 -0600

    add flake, update gitignore for direnv

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "zcash-t2o: Transparent-to-Orchard PCZT Library for Go";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            rustc
+            cargo
+            pkg-config
+            gcc
+          ];
+
+          shellHook = ''
+            export CGO_ENABLED=1
+          '';
+        };
+      }
+    );
+}
